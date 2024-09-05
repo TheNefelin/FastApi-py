319798d46843bb95e54c35f98ca527585f65062a
Francisco Carmona <flcarmonac@yahoo.com>
2024-09-05 12:19:25 -0400

    primitive test

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -8,7 +8,7 @@ from app.mssql import execute_sp
 
 TOKEN_SECRET_KEY = os.getenv("TOKEN_SECRET_KEY")
 TOKEN_ALGORITHM = os.getenv("TOKEN_ALGORITHM")
-TOKEN_TIMEOUT = os.getenv("TOKEN_TIMEOUT")
+TOKEN_TIMEOUT = int(os.getenv("TOKEN_TIMEOUT"))
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
@@ -28,15 +28,7 @@ class TokenData(BaseModel):
 def create_access_token(data: dict):
   to_encode = data.copy()
 
-  if TOKEN_TIMEOUT is not None:
-    try:
-      expires_delta = timedelta(minutes=int(TOKEN_TIMEOUT))
-    except ValueError:
-      expires_delta = timedelta(minutes=15)  # Valor predeterminado en caso de error
-    else:
-      expires_delta = timedelta(minutes=15) 
-
-  expires_delta = timedelta(minutes = int(TOKEN_TIMEOUT))
+  expires_delta = timedelta(minutes = TOKEN_TIMEOUT)
 
   if expires_delta:
     expire = datetime.now().astimezone() + expires_delta
